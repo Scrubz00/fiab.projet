3d654f6ae6ba79d1eb2538fd8c0c98faff2d8a8a
Scrubz00 <younes.chahma@gmail.com>
2020-11-13 16:59:12 +0100

    modif

--- script.R
+++ script.R
@@ -7,5 +7,65 @@ phi <- function(x){
   return(rep)
 }
 
+# question 2
+
+phi_function <- function(lambda, beta, t){
+  R1 <- 1-pweibull(t, shape = beta, scale = lambda)
+  R2 <- 1-pweibull(t, shape = beta, scale = lambda)
+  R3 <- 1-pweibull(t, shape = beta, scale = lambda)
+  R4 <- 1-pweibull(t, shape = beta, scale = lambda)
+  R5 <- 1-pweibull(t, shape = beta, scale = lambda)
+  R6 <- 1-pweibull(t, shape = beta, scale = lambda)
+  R7 <- 1-pweibull(t, shape = beta, scale = lambda)
+  R8 <- 1-pweibull(t, shape = beta, scale = lambda)
+  R9 <- 1-pweibull(t, shape = beta, scale = lambda)
+  rep <- ((R3[t]*(1-(1-R1[t])*(1-R2[t]))*(1-(1-R4[t])*(1-R5[t])))+(1-R3[t])*(1-(1-R1[t]*R4[t])*(1-R2[t]*R5[t])))*(1-(1-R6[t]*R7[t])*(1-R8[t])*(1-R9[t]))
+  return(rep)
+}
+
+
+
+
 ## Question 3
+
+plot(phi_function(20,2, 0:35), type = 'l')
+
+## Question 4
+
+survie <- function(lambda, beta, t){
+  R1 <- 1-pweibull(t, shape = beta, scale = lambda)
+  R2 <- 1-pweibull(t, shape = beta, scale = lambda)
+  R3 <- 1-pweibull(t, shape = beta, scale = lambda)
+  R4 <- 1-pweibull(t, shape = beta, scale = lambda)
+  R5 <- 1-pweibull(t, shape = beta, scale = lambda)
+  R6 <- 1-pweibull(t, shape = beta, scale = lambda)
+  R7 <- 1-pweibull(t, shape = beta, scale = lambda)
+  R8 <- 1-pweibull(t, shape = beta, scale = lambda)
+  R9 <- 1-pweibull(t, shape = beta, scale = lambda)
+  rep <- -log(((R3[t]*(1-(1-R1[t])*(1-R2[t]))*(1-(1-R4[t])*(1-R5[t])))+(1-R3[t])*(1-(1-R1[t]*R4[t])*(1-R2[t]*R5[t])))*(1-(1-R6[t]*R7[t])*(1-R8[t])*(1-R9[t]))/t)
+  # rep <- -log(rep)/t
+  return(rep)
+  # rep <- -log(1 - phi_function(lambda, beta, t))/t
+}
+
+survie <- function(lambda, beta, t){
+  rep=NULL
+  for(i in 1:length(t)){
+    R1 <- 1-pweibull(t[i], shape = beta, scale = lambda)
+    R2 <- 1-pweibull(t[i], shape = beta, scale = lambda)
+    R3 <- 1-pweibull(t[i], shape = beta, scale = lambda)
+    R4 <- 1-pweibull(t[i], shape = beta, scale = lambda)
+    R5 <- 1-pweibull(t[i], shape = beta, scale = lambda)
+    R6 <- 1-pweibull(t[i], shape = beta, scale = lambda)
+    R7 <- 1-pweibull(t[i], shape = beta, scale = lambda)
+    R8 <- 1-pweibull(t[i], shape = beta, scale = lambda)
+    R9 <- 1-pweibull(t[i], shape = beta, scale = lambda)
+    rep[i] <- -log(((R3*(1-(1-R1)*(1-R2))*(1-(1-R4)*(1-R5)))+(1-R3)*(1-(1-R1*R4)*(1-R2*R5)))*(1-(1-R6*R7)*(1-R8)*(1-R9)))/t[i]
+  }
+  return(rep)
+}
+
+
+survie(20, 2, seq(0.1, 20, 0.1))
+plot(survie(20, 2, seq(0.1, 20, 0.1)), type = 'l')
 
